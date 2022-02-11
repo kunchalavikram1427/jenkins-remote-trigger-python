2b0ce23e6653c3cae18328cf4e0b7f7fb091b5b3
vikram.k <vikram@gmail.com>
2022-02-11 15:04:56 +0530

    Modified for builf=d with parameters

--- remote-trigger.py
+++ remote-trigger.py
@@ -16,16 +16,22 @@ try:
     user_token = jsonConfigContent['user_token']
     job_name = jsonConfigContent['job_name']
     job_token = jsonConfigContent['job_token']
+    isTheJobParametrized = jsonConfigContent['isTheJobParametrized']
 
 except Exception as e:
     print(f"[Exception]: In using the configuration file: {e}")
 
 # Form the URL
-url = jenkins_url+'/job/'+job_name+'/build?token='+job_token
-print(f"[Info]: Triggering the job: {url}")
+if not isTheJobParametrized:
+    url = jenkins_url+'/job/'+job_name+'/build?token='+job_token
+    my_data = None
+else:
+    url = jenkins_url+'/job/'+job_name+'/buildWithParameters?token='+job_token
+    my_data = jsonConfigContent['my_data']
 
+print(f"[Info]: Triggering the job: {url}")
 # Send the HTTP Request
-result = requests.post(url, auth = (user, user_token))
+result = requests.post(url, auth = (user, user_token), data=my_data)
 
 if int(result.status_code) != 201: 
     print(f"[Error]: Triggering remote job failed with status_code: {result.status_code}")
